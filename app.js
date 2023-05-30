24f318aa92909f300b1c0b614961e9535748409e
SaiIndra2003 <98470966+SaiIndra2003@users.noreply.github.com>
2023-05-30 12:41:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,19 +25,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  //We would set the header of response (Server) here so that cors error is handled
-  res.set("Access-Control-Allow-Origin", "*"); //in * you can give url's to which only our server connects (cors error) (Giving access of this server to specified urls * indicates all url's)
+  res.set("Access-Control-Allow-Origin", "*");  
   res.set(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  ); // Allows the access to the specified types of headers
+  );
   if (req.method === "OPTIONS") {
-    // Options req is send by browser whenever put/patch/delete/post method is done
     res.set("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
   }
   next();
-  //If not mentioned the server will end it here and the handler will not able to catch the remainming we mentioned
 });
 
 app.use("/products", productsRoute);
